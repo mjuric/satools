622fde4e9517aeee48abe431b17c59a5877f457d
Mario Juric <mjuric@ias.edu>
2008-01-31 02:41:40 -0500

    More cleanups.

--- src/stuff.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <fstream>
-#include <iostream>
-
-#include "stuff.h"
-
--- src/test.f
+++
@@ -1,5 +0,0 @@
-	SUBROUTINE test(a, text, b)
-	character*(*) text
-	double precision a, b
-	write(*,*) text, a, b
-	END
